f1956bb92e91495a6bd1a82f05737facd6be832c
absandell <absandell@gmail.com>
2021-10-17 20:01:43 -0400

    Adds local view display

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const express = require('express');
 
 const app = express();
 
+// Set up view engine
 app.set('view engine', 'ejs');
+
+// Create home route
+app.get('/', (req, res) => {
+    res.render('home')
+})
 
 app.listen(3000, ()=> {
     console.log('app now listening for requests on port 3000')
