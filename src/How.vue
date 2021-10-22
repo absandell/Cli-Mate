c7ffbc643631a8f8f49cf4b1fc05a26e2833a762
christopherdang <58712502+christopherdang@users.noreply.github.com>
2021-10-22 11:29:00 -0400

    Updated background images on all pages and slight layout to App.Vue

--- src/How.vue
+++ src/How.vue
@@ -1,21 +1,23 @@
 <template>
-    <HomeButton class='btnHome'/>
-    <h1 class='title'>How to Use Cli-Mate</h1>
-    <h1 class='body'>The purpose of the application is to teach you how to decrease carbon emissions and keep track of their progress.</h1>
-    <br>
-    <h1 class='body'>Throughout the day, log your transportation activities by:</h1>
-    <h1 class='body'>1. Specifying the mode of transportation</h1>
-    <h1 class='body'>2. Entering where "from" and "to" you traveled</h1>
-    <h1 class='body'>3. Submitting the activity</h1>
-    <br>
-    <h1 class='body'>Based off your activity history, we will calculate your performance and show how you have impacted the planet.</h1>
-    <h1 class='body'>Your status will be displayed visually as the Earth, where the health-state of the Earth will reflect your performance.</h1>
-    <h1 class='body'>More transportation using automobiles will negatively affect the Earth, lowering your performance and showing a more sickly Earth.</h1>
-    <h1 class='body'>Whereas transportation by walking/biking will positively affect the Earth, increasing your performance and showing a more healthy Earth.</h1>
-    <br>
-    <h1 class='body'>You can view the "General Information" section to view tips and other ways to help the environment.</h1>
-    <br>
-    <h1 class='body'>Hope you enjoy the app and lets save the Earth together!</h1>
+    <div class='bg'>
+        <HomeButton class='btnHome'/>
+        <h1 class='title'>How to Use Cli-Mate</h1>
+        <h1 class='body'>The purpose of the application is to teach you how to decrease carbon emissions and keep track of their progress.</h1>
+        <br>
+        <h1 class='body'>Throughout the day, log your transportation activities by:</h1>
+        <h1 class='body'>1. Specifying the mode of transportation</h1>
+        <h1 class='body'>2. Entering where "from" and "to" you traveled</h1>
+        <h1 class='body'>3. Submitting the activity</h1>
+        <br>
+        <h1 class='body'>Based off your activity history, we will calculate your performance and show how you have impacted the planet.</h1>
+        <h1 class='body'>Your status will be displayed visually as the Earth, where the health-state of the Earth will reflect your performance.</h1>
+        <h1 class='body'>More transportation using automobiles will negatively affect the Earth, lowering your performance and showing a more sickly Earth.</h1>
+        <h1 class='body'>Whereas transportation by walking/biking will positively affect the Earth, increasing your performance and showing a more healthy Earth.</h1>
+        <br>
+        <h1 class='body'>You can view the "General Information" section to view tips and other ways to help the environment.</h1>
+        <br>
+        <h1 class='body'>Hope you enjoy the app and lets save the Earth together!</h1>
+    </div>
 </template>
 
 <script>
@@ -181,5 +183,13 @@ body {
   font-family: inherit;
   top: 0%;
   left: 45%;
+}
+.bg {
+   border: 2px solid black;
+  padding: 100px;
+  padding-bottom: 100px;
+  background: url(./assets/bg1.png);
+  background-repeat: no-repeat;
+  background-size: cover;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import Login from './Activities.vue'
+import Login from './App.vue'
 import router from './router'
 
 createApp(Login).use(router).mount('#login')
